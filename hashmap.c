96ff6532d8527c5422e4c3b3cb5777e388261fc5
Replit user <>
2022-09-10 18:50:36 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -50,7 +50,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 while(map->buckets[i] != NULL && map->buckets[i]->key != NULL){
   i = (i+1) % map->capacity;
   }
-  map->buckets[i];
+  map->buckets[i] = new;
   map->current = i;
   map->size++;
 }
