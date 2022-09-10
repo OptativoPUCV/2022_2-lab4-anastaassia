3f4ebbff266f61d0cb0a8a6ee8687338c4ea78cd
Replit user <>
2022-09-10 19:30:38 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -67,12 +67,12 @@ void enlarge(HashMap * map) {
 HashMap * createMap(long capacity) {
   HashMap *map = (HashMap *) malloc(sizeof(HashMap));
 
- map->buckets = (Pair **) calloc(capacity , sizeof(Pair *));
- map->capacity = capacity;
- map->size = -1;
- map->size = 0;
+    map->buckets = (Pair **) calloc(capacity, sizeof(Pair *));
+    map->capacity = capacity;
+    map->current = -1;
+    map->size = 0;
 
-   return map;
+    return map;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
